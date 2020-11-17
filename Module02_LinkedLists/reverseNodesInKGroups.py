82d4360bbf271acc16ad78f14d8ec197e580a5e2
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-17 17:03:40 -0500

    revNodeInKGroups working

--- Module02_LinkedLists/reverseNodesInKGroups.py
+++ Module02_LinkedLists/reverseNodesInKGroups.py
@@ -48,30 +48,128 @@ class ListNode(object):
     self.value = value
     self.next = None
 
-def reverseNodesInKGroups(l, k):
-    # keep track of a current node
 
-    # move (k nodes) nodes across to see if our sub-list size would be long enough
-    # iterate over a range of k
-        # check if we run out of nodes
-            # just return l
-        # increment our current node pointer/ref
+"""RecursionError: maximum recursion depth exceeded in comparison"""
+# def reverseNodesInKGroups(l, k):
+#     # keep track of a current node
+#     current_node = l
 
-    # set a reveral ref to l
-    # set current to l.next
+#     # move (k nodes) nodes across to see if our sub-list size would be long enough
+#     # iterate over a range of k
+#     for i in range(k):
+#         # check if we run out of nodes
+#         if not current_node:
+#             # just return l
+#             return l
+#         # increment our current node pointer/ref
+#         current_node = current_node.next
 
-    # swap the next k elements
-    # iterate to k - 1
-        # set current next set it to reversal
-        # set current to current next (increment current)
-        # set my reversal to the current
+#     # set a reveral ref to l
+#     rev = l
+#     # set current to l.next
+#     current_node = l.next
+
+#     # swap the next k elements
+#     # iterate to k - 1
+#     for i in range(k - 1):
+#         # set current next to reversal
+#         current_node.next = rev
+#         # set current to current next (increment current)
+#         current_node = current_node.next
+#         # set my reversal to the current
+#         rev = current_node
+
+#     # recursive call
+#     # connect the head node with the rest of the reversed sub lists
+#     l.next = reverseNodesInKGroups(current_node,k)
+
+#     #return our reversal
+#     return ListNode(0)
 
 
-    # recursive call
-    # connect the head node with the rest of the reversed sub lists
+"""new Solution"""
 
-    #return our reversal
-    return ListNode(0)
+# some sort of helper function
+def reverse(start, end):
+    """
+    takes in 2 nodes a start and end
+    swaps all nodes
+    iterating over start to end
+    """
+    
+    # set a prev to the end
+    prev = end
+    # set a next_starting_node to the start.next
+    next_starting_node = start.next
+    # set a current_node
+    current_node = start.next
+
+    # while the current_node is not the end
+    while current_node != end:
+        # swap things
+        # set a temp to current nodes next
+        temp = current_node.next
+        # set current nodes next to prev
+        current_node.next = prev
+        # set prev to current node
+        prev = current_node
+        # set current node to temp
+        current_node = temp
+
+    # set our starting node next to prev
+    start.next = prev
+
+    # return our next starting node to the caller
+    return next_starting_node
+
+
+def reverseNodesInKGroups(l,k):
+    # check base case and end cases
+    
+    # if l is none
+    # of if l.next is none
+    # or if k == l
+    if l is None or l.next is None or k==1:
+        # return l
+        return l
+    
+    # edge case to swap the first item (create a new list)
+    list = ListNode(0) # just a placeholder / temporary head
+    
+    # apppend list l to the end of this list
+    list.next = l
+    
+    # set list to the head
+    starting_node = list
+
+    # keep track of some counter
+    counter = 0
+
+    # iterate over the whole list
+    while l:
+        # increment my counter
+        counter +=1
+
+        # check if counter mod k is zero
+        if counter % k == 0:  # for each k group    
+            # set starting node to rev of starting node and l.next
+            # (call to helper function)
+            starting_node = reverse(starting_node, l.next)
+            
+            # set l (the head) to the starting nodes next
+            l = starting_node.next
+        # otherwise, 
+        else:
+            # traverse forward setting l to l.next
+            l = l.next
+    # return the list.next    
+    return list.next
+
+
+
+
+    #  
+    pass
 
 
 def print_list(l):
